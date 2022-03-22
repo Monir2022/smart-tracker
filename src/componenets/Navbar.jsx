b1d66222c6bd86055b5354b3e20ab061fa774921
Monir Akhand <akhandbuet@gmail.com>
2022-03-22 17:31:51 +0100

    logo has imported

--- src/componenets/Navbar.jsx
+++ src/componenets/Navbar.jsx
@@ -1,11 +1,13 @@
 //NPM packages
 import { Link } from "react-router-dom";
+//Project file
+import logo from "../assets/images/logo.png"
 
 export default function Navbar() {
   return (
     <div className="navbar">
       <Link className="logo" to="/">
-        <img className="logo-img" src={""} alt="logo thumbnail" />
+        <img className="logo-img" src={logo} alt="logo thumbnail" />
       </Link>
       <ul className="navbar-link">
         <li>
