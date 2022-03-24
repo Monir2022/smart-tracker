b5d77a42513686ec723ce03204de8f1e12ebba93
Monir Akhand <akhandbuet@gmail.com>
2022-03-24 22:14:21 +0100

    Navbar css has added

--- src/componenets/Navbar.jsx
+++ src/componenets/Navbar.jsx
@@ -1,21 +1,25 @@
-import React, { useContext }  from 'react';
+import React, { useContext } from "react";
 import { LanContext } from "../contexts/LanContext";
 import { Link } from "react-router-dom";
 //Project file
 import logo from "../assets/images/logo.png";
 
-import Language from './Language';
+import Language from "./Language";
 
 export default function Navbar() {
-  const {lan} = useContext(LanContext);
+  const { lan } = useContext(LanContext);
 
   return (
     <nav className="navbar">
       <Link className="logo" to="/">
         <img className="logo-img" src={logo} alt="logo thumbnail" />
       </Link>
-      <Link to="/">{lan.Home}</Link>
-      <Link to="/track">{lan.Track}</Link>
+      <Link className="links" to="/">
+        {lan.Home}
+      </Link>
+      <Link className="links" to="/track">
+        {lan.Track}
+      </Link>
       <Language />
     </nav>
   );
